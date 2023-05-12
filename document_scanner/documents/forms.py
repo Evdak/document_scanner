ccd1467102dfcc05d4bfe8a5a91d20102099dd39
Evdak <57111036+Evdak@users.noreply.github.com>
2023-05-12 16:54:31 +0300

    fix multiple files form

--- document_scanner/documents/forms.py
+++ document_scanner/documents/forms.py
@@ -3,10 +3,28 @@ from django.forms import ClearableFileInput
 from .models import UploadFiles
 
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
 class FileUpload(forms.ModelForm):
     class Meta:
         model = UploadFiles
-        fields = ['files']
+        fields = ["files"]
         widgets = {
-            'files': ClearableFileInput(attrs={'multiple': True}),
+            "files": MultipleFileField(),
         }
